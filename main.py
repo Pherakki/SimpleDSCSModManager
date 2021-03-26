cdf9c23be63d4a41bec0519b00df786e3f23852d
Pherakki <pherakki@gmail.com>
2021-03-26 00:30:57 +0000

    Remove the conflict graph for now, until the code can be rewritten in terms of mod indices

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ class MainWindow(QtWidgets.QMainWindow):
         # Hook the UI
         self.ui.hook_menu(open_patreon)
         self.ui.hook_profle_interaction_widgets(self.profile_handler)
-        self.ui.hook_action_tabs(self.draw_conflicts_graph)
+        #self.ui.hook_action_tabs(self.draw_conflicts_graph)
         self.ui.hook_config_tab(self.find_gamelocation, self.update_dscstools)
         self.ui.hook_extract_tab(self.dscstools_dump_factory)
         self.ui.hook_mod_registry(self.register_mod)
@@ -183,43 +183,43 @@ class MainWindow(QtWidgets.QMainWindow):
         return retval
     
         
-    def draw_conflicts_graph(self, tabIdx):
-        if tabIdx == 2:    
-            self.ui.conflicts_graph.model().clear()
-            mods = self.profile_handler.get_active_mods()
-            graph = generate_patched_mods(mods, self.output_loc)  
-            graph = prepare_graph_for_display(graph)
-            self.ui.conflicts_graph.setAlternatingRowColors(True)
-            header_labels = ["File", *[mod.name for mod in mods], "Used Mod"]
-            self.ui.conflicts_graph.model().setHorizontalHeaderLabels(header_labels)
-            self.draw_conflicts_subgraph(graph, mods)
+    # def draw_conflicts_graph(self, tabIdx):
+    #     if tabIdx == 2:    
+    #         self.ui.conflicts_graph.model().clear()
+    #         mods = self.profile_handler.get_active_mods()
+    #         graph = generate_patched_mods(mods, self.output_loc)  
+    #         graph = prepare_graph_for_display(graph)
+    #         self.ui.conflicts_graph.setAlternatingRowColors(True)
+    #         header_labels = ["File", *[mod.name for mod in mods], "Used Mod"]
+    #         self.ui.conflicts_graph.model().setHorizontalHeaderLabels(header_labels)
+    #         self.draw_conflicts_subgraph(graph, mods)
                 
-    def draw_conflicts_subgraph(self, graph, mods, parent=None):
-        colours = {0: QtGui.QColor( 150,  150,  150),
-           1: QtGui.QColor(  0,   0,   0),
-           2: QtGui.QColor(255, 255,   0),
-           3: QtGui.QColor(  0,   0, 255),
-           4: QtGui.QColor(255,   0,   0),
-           5: QtGui.QColor(200, 255, 200),
-           6: QtGui.QColor(  0, 255,   0),
-           7: QtGui.QColor(  0, 255, 255)}
+    # def draw_conflicts_subgraph(self, graph, mods, parent=None):
+    #     colours = {0: QtGui.QColor( 150,  150,  150),
+    #        1: QtGui.QColor(  0,   0,   0),
+    #        2: QtGui.QColor(255, 255,   0),
+    #        3: QtGui.QColor(  0,   0, 255),
+    #        4: QtGui.QColor(255,   0,   0),
+    #        5: QtGui.QColor(200, 255, 200),
+    #        6: QtGui.QColor(  0, 255,   0),
+    #        7: QtGui.QColor(  0, 255, 255)}
         
-        if parent is None:
-            parent = self.ui.conflicts_graph.model().invisibleRootItem()
+    #     if parent is None:
+    #         parent = self.ui.conflicts_graph.model().invisibleRootItem()
         
-        for j, (key, value) in enumerate(graph.items()):
-            nameitem = QtGui.QStandardItem(key)
-            modelitem = [nameitem, *[QtGui.QStandardItem() for _ in mods], QtGui.QStandardItem()]
-            for i in range(len(mods)):
-                if type(value) != dict:
-                    modelitem[i+1].setData(colours[value[i]], QtCore.Qt.BackgroundRole)
-                #else:
-                #    modelitem[i+1].setData(colours[0], QtCore.Qt.BackgroundRole)
-            for item in modelitem:                
-                item.setEditable(False)
-            parent.appendRow(modelitem)
-            if type(value) == dict:
-                self.draw_conflicts_subgraph(value, mods, nameitem)
+    #     for j, (key, value) in enumerate(graph.items()):
+    #         nameitem = QtGui.QStandardItem(key)
+    #         modelitem = [nameitem, *[QtGui.QStandardItem() for _ in mods], QtGui.QStandardItem()]
+    #         for i in range(len(mods)):
+    #             if type(value) != dict:
+    #                 modelitem[i+1].setData(colours[value[i]], QtCore.Qt.BackgroundRole)
+    #             #else:
+    #             #    modelitem[i+1].setData(colours[0], QtCore.Qt.BackgroundRole)
+    #         for item in modelitem:                
+    #             item.setEditable(False)
+    #         parent.appendRow(modelitem)
+    #         if type(value) == dict:
+    #             self.draw_conflicts_subgraph(value, mods, nameitem)
             
 
     def update_mods(self):
