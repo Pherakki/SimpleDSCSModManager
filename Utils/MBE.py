f945238735e10e0422803203b937d1fd87089017
Pherakki <pherakki@gmail.com>
2021-03-27 02:16:50 +0000

    Add an mbe unpack utility (could unify with batch pack in the future)

--- Utils/MBE.py
+++ Utils/MBE.py
@@ -18,3 +18,22 @@ def mbe_batch_pack(path, dscstools_handler, log=lambda x: x):
                 os.rename(os.path.join(temp_path, folder), os.path.join(mbe_folder_path, folder))
     os.rmdir(temp_path)
     
+def mbe_batch_unpack(path, dscstools_handler, log=lambda x: x, updateLog=lambda x: x):
+    temp_path = os.path.join(path, 'temp')
+    os.mkdir(temp_path)
+    for mbe_folder in ['data', 'message', 'text']:
+        mbe_folder_path = os.path.join(path, mbe_folder)
+        if os.path.exists(mbe_folder_path):
+            log(f"Unpacking MBEs in {mbe_folder}...")
+            log("")  # <- Will be overwritten by updateLog
+            num_mbes = len(os.listdir(mbe_folder_path))
+            for i, folder in enumerate(os.listdir(mbe_folder_path)):
+                # Generate the mbe inside the 'temp' directory
+                dscstools_handler.unpack_mbe(folder, 
+                                           os.path.abspath(mbe_folder_path), 
+                                           os.path.abspath(temp_path))
+                os.remove(os.path.join(mbe_folder_path, folder))
+                # Move the unpacked MBE out out 'temp' and into the correct path
+                shutil.move(os.path.join(temp_path, folder), os.path.join(mbe_folder_path, folder))
+                updateLog(f"Unpacked {i+1}/{num_mbes} [{folder}]")
+    os.rmdir(temp_path)
