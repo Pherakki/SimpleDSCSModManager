c7380bbc58301efe35e2272b7c712e87e80e8808
Pherakki <pherakki@gmail.com>
2021-04-08 16:25:47 +0100

    Add option to remove input scripts for compile/decompile

--- ToolHandlers/ScriptHandler.py
+++ ToolHandlers/ScriptHandler.py
@@ -10,16 +10,19 @@ class ScriptHandler:
         self.compiler_location = compiler_location
         self.compiler_directory = os.path.split(compiler_location)[0]
         
-    def compile_script(self, file, origin, destination):
+    def compile_script(self, file, origin, destination, remove_input=False):
         filename = os.path.splitext(file)[0]
         p = subprocess.run([self.compiler_location, "-o", os.path.join(destination, filename) + '.nut', "-c", os.path.join(origin, file)], 
                              creationflags=subprocess.CREATE_NO_WINDOW, cwd=self.compiler_directory, close_fds=True)
-
+        if remove_input:
+            os.remove(os.path.join(origin, file))
             
-    def decompile_script(self, file, origin, destination):
+    def decompile_script(self, file, origin, destination, remove_input=False):
         filename = os.path.splitext(file)[0]
         out_path = os.path.join(destination, filename) + '.txt'
         with open(out_path, "w") as out:
             p = subprocess.run([self.nutcracker_location, f"{os.path.join(origin, file)}"], 
                                   creationflags=subprocess.CREATE_NO_WINDOW, cwd=self.nutcracker_directory,
                                   stdout=out, close_fds=True)
+            if remove_input:
+                os.remove(os.path.join(origin, file))
