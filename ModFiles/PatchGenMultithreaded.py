8f4801c321f9d7c4690cf5d86979a57742f50597
Pherakki <pherakki@gmail.com>
2021-04-10 23:50:36 +0100

    Fix implementation of PoolChain

--- ModFiles/PatchGenMultithreaded.py
+++ ModFiles/PatchGenMultithreaded.py
@@ -11,13 +11,17 @@ class PoolChain(QtCore.QObject):
     finished = QtCore.pyqtSignal()
     
     def __init__(self, *pools):
+        super().__init__()
         self.pools = pools
         for pool_1, pool_2 in zip(pools, pools[1:]):
+        for pool_1, pool_2 in zip(self.pools, self.pools[1:]):
             pool_1.finished.connect(pool_2.run)
         pools[-1].finished.connect(self.finished.emit)
+        self.pools[-1].finished.connect(self.finished.emit)
         
     def run(self):
         pools[0].run()
+        self.pools[0].run()
     
 
 class generate_patch_mt(QtCore.QObject):
