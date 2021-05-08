ef7206a71cb0bab468cd66d979787022ae755988
Pherakki <pherakki@gmail.com>
2021-05-08 02:03:35 +0100

    Factor out PoolChain

--- ModFiles/PatchGenMultithreaded.py
+++ ModFiles/PatchGenMultithreaded.py
@@ -5,20 +5,10 @@ import shutil
 from PyQt5 import QtCore
 
 from .ScriptPatching import patch_scripts
+from Utils.Multithreading import PoolChain
 from Utils.Path import splitpath
 
-class PoolChain(QtCore.QObject):
-    finished = QtCore.pyqtSignal()
     
-    def __init__(self, *pools):
-        super().__init__()
-        self.pools = pools
-        for pool_1, pool_2 in zip(self.pools, self.pools[1:]):
-            pool_1.finished.connect(pool_2.run)
-        self.pools[-1].finished.connect(self.finished.emit)
-        
-    def run(self):
-        self.pools[0].run()
     
 
 class generate_patch_mt(QtCore.QObject):
