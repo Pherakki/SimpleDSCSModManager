ac16ed4077acb44f7a4f0a45a7fb002fbf7dd95b
Pherakki <pherakki@gmail.com>
2021-05-10 20:09:34 +0100

    Fix evorecord_join

--- plugins/rules/mbe.py
+++ plugins/rules/mbe.py
@@ -1,6 +1,12 @@
 def mberecord_overwrite(record_id, mbe_data, mod_mbe_data):
     mbe_data[record_id] = mod_mbe_data[record_id]
     
-def mberecord_join(record_id, mbe_data, mod_mbe_data):
-    new_data = [elem for elem in mod_mbe_data[record_id] if elem not in mbe_data[record_id]]
-    mbe_data[record_id].extend(new_data)
+def evorecord_join(record_id, mbe_data, mod_mbe_data):
+    max_records = 6
+    nonzero_data = [elem for elem in mbe_data.get(record_id, []) if elem != '0']
+    new_data = [elem for elem in mod_mbe_data[record_id] if elem not in nonzero_data]
+    nonzero_data.extend(new_data)
+    nonzero_data = nonzero_data[:max_records]
+    # Pad out the records such that there are always 6 evolution entries + the ID
+    nonzero_data.extend(['0']*(max_records - len(nonzero_data)))
+    mbe_data[record_id] = nonzero_data
