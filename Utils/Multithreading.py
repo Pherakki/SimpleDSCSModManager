7bef8240bb672e11dd7b30f9b2d33523f634f97b
Pherakki <pherakki@gmail.com>
2021-05-23 22:16:18 +0100

    Fix bug where PoolRunner will crash if no pools are passed

--- Utils/Multithreading.py
+++ Utils/Multithreading.py
@@ -8,10 +8,14 @@ class PoolChain(QtCore.QObject):
     def __init__(self, *pools):
         super().__init__()
         self.pools = pools
-        for pool_1, pool_2 in zip(self.pools, self.pools[1:]):
-            pool_1.finished.connect(pool_2.run)
-        self.pools[-1].finished.connect(self.finished.emit)
+        if len(pools):
+            for pool_1, pool_2 in zip(self.pools, self.pools[1:]):
+                pool_1.finished.connect(pool_2.run)
+            self.pools[-1].finished.connect(self.finished.emit)
         
     def run(self):
-        self.pools[0].run()
         self.lockGui.emit()
+        if len(self.pools):
+            self.pools[0].run()
+        else:
+            self.finished.emit()
