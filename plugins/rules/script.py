c7feeb2aadf6079d51246ecf8f59df5794a6704a
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-03-24 23:27:45 +0000

    Update script rule to be a class

--- plugins/rules/script.py
+++ plugins/rules/script.py
@@ -1,13 +1,28 @@
 import os
 
+from Utils.Settings import default_encoding
+from Utils.Softcodes import replace_softcodes
 # from ModFiles.ScriptPatching import patch_scripts
 
 
-def squirrel_concat(source_code, script_filepath):
-    with open(script_filepath, 'r') as F:
-        mod_source_code = F.read()
-    source_code += "\n// CONCAT INPUT FILE: \n\n"
-    source_code += mod_source_code
+class squirrel_concat:
+    overrides_all_previous = False
+    is_anchor = True
+    is_solitary = False
     
-    return source_code
+    group = "Script"
+    
+    def __call__(self, build_data):
+        script_filepath = build_data.source
+        source_code     = build_data.source_code
+        softcodes       = build_data.softcodes
+        softcode_lookup = build_data.softcode_lookup
+        
+        with open(script_filepath, 'rb') as F:
+            mod_source_code = replace_softcodes(F.read(), softcodes, softcode_lookup).decode(default_encoding)
+        source_code += "\n// CONCAT INPUT FILE: \n\n"
+        source_code += mod_source_code
+        
+        
+        return source_code
     
