ad31e8f2a0b726d57a591b2c79b3e7cbec503a9f
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-03-24 23:27:27 +0000

    Update sqmod rule to be a class

--- plugins/rules/sqmod.py
+++ plugins/rules/sqmod.py
@@ -1,9 +1,26 @@
 import os
 import json
 
+from Utils.Settings import default_encoding
+from Utils.Softcodes import replace_softcodes
 from Utils.SqModImpl import modify_squirrel_source
 
-def squirrel_modify(source_code, sqmod_path):
-    with open(sqmod_path, 'r') as F:
-        modifications = json.load(F)
-    return modify_squirrel_source(source_code, modifications)
+class squirrel_modify:
+    overrides_all_previous = False
+    is_anchor = True
+    is_solitary = False
+    
+    group = "Script"
+    
+    def __call__(self, build_data):
+        sqmod_path      = build_data.source
+        source_code     = build_data.source_code
+        softcodes       = build_data.softcodes
+        softcode_lookup = build_data.softcode_lookup
+        
+        with open(sqmod_path, 'rb') as F:
+            modifications = replace_softcodes(F.read(), softcodes, softcode_lookup)
+            modifications = json.loads(modifications.decode(default_encoding))
+        return modify_squirrel_source(source_code, modifications)
+
+# overwrite
