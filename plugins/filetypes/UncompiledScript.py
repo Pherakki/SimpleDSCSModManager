305c3ffe8244962093aa23179d1a354c6f8654d0
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-03-25 17:32:01 +0000

    Update the script filetype to use the new base class types

--- plugins/filetypes/UncompiledScript.py
+++ plugins/filetypes/UncompiledScript.py
@@ -1,8 +1,14 @@
 import os
+import sys
 
-class UncompiledScript:
-    group = 'script_src'
-    default_rule = 'squirrel_concat'
+from CoreOperations.PluginLoaders.FiletypesPluginLoader import BaseBuildElement, BaseFiletype
+
+
+class UncompiledScriptBuildElement(BaseBuildElement):
+    __slots__ = tuple()
+    
+    build_element_id = sys.intern('script_src')
+    default_rule =  sys.intern('squirrel_concat')
     enable_softcodes = True
     
     @staticmethod
@@ -20,11 +26,25 @@ class UncompiledScript:
     def get_rule(cls, filepath):
         return cls.default_rule
     
-    @classmethod
-    def produce_index(cls, path, filename):
-        return cls.group, os.path.join(path, filename), [filename]
-    
     @staticmethod
     def get_pack_name(filepath):
         return filepath
+    
+    @classmethod
+    def get_filetype_cls(cls):
+        return UncompiledScript
         
+
+class UncompiledScript(BaseFiletype):
+    __slots__ = tuple()
+    
+    build_elements = [UncompiledScriptBuildElement]
+    filetype_id =  sys.intern("script_src")
+    filepack = sys.intern("Script")
+    
+    @staticmethod
+    def checkIfMatch(path, filename):
+        if os.path.split(path)[-1] == 'script64' and os.path.splitext(filename)[-1] == '.txt':
+            return True
+        else:
+            return False
