9a81a032eb15aeeb0152f0b5c12e9b1a4de2f9a0
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-03-25 17:56:47 +0000

    Update getters for rule plugins

--- CoreOperations/PluginLoaders/RulesPluginLoader.py
+++ CoreOperations/PluginLoaders/RulesPluginLoader.py
@@ -1,15 +1,16 @@
 import inspect
 import os
-import shutil
 
 from CoreOperations.PluginLoaders.PluginLoad import load_plugins_in
 
 
-def get_rule_plugins():
+def get_rule_plugins(*groups):
+    rule_categories = set(groups)
     plugin_dir = os.path.join('plugins', 'rules')
-    rules = load_plugins_in(plugin_dir, inspect.isfunction)
-    return {rule.__name__: rule for rule in [*rules, overwrite]}
-
-
 def overwrite(src, dst):
     shutil.copy2(src, dst)
+    rules = load_plugins_in(plugin_dir, inspect.isclass)
+    if len(rule_categories):
+        return {rule.__name__: rule() for rule in rules if rule.group in rule_categories}
+    else:
+        return {rule.__name__: rule() for rule in rules}
