2f672f6b979511f3d86233d43e6004ebcf52c71e
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-03-25 17:56:56 +0000

    Remove useless overwrite function

--- CoreOperations/PluginLoaders/RulesPluginLoader.py
+++ CoreOperations/PluginLoaders/RulesPluginLoader.py
@@ -7,8 +7,6 @@ from CoreOperations.PluginLoaders.PluginLoad import load_plugins_in
 def get_rule_plugins(*groups):
     rule_categories = set(groups)
     plugin_dir = os.path.join('plugins', 'rules')
-def overwrite(src, dst):
-    shutil.copy2(src, dst)
     rules = load_plugins_in(plugin_dir, inspect.isclass)
     if len(rule_categories):
         return {rule.__name__: rule() for rule in rules if rule.group in rule_categories}
