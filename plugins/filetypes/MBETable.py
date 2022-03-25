ad536bc7662545952418ab711fcecc0c6690891d
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-03-25 17:29:30 +0000

    Update the MBETable filetype to use the new base class types

--- plugins/filetypes/MBETable.py
+++ plugins/filetypes/MBETable.py
@@ -1,19 +1,12 @@
-import csv
-import json
 import os
-from Utils.Path import splitpath
+from CoreOperations.PluginLoaders.FiletypesPluginLoader import BaseBuildElement, BaseFiletype
 
-class MBE_table:
-    group = 'mbe'
+class MBETableBuildElement(BaseBuildElement):
+    __slots__ = tuple()
+    
+    build_element_id = 'mbe'
     default_rule = 'mberecord_merge'
     enable_softcodes = True
-    
-    @staticmethod
-    def checkIfMatch(path, filename):
-        if os.path.split(path)[-1].split('.')[-1] == 'mbe' and os.path.splitext(filename)[-1] == '.csv':
-            return True
-        else:
-            return False
         
     @staticmethod
     def get_target(filepath):
@@ -22,20 +15,26 @@ class MBE_table:
     @classmethod
     def get_rule(cls, filepath):
         return cls.default_rule
-        
-    # @classmethod
-    # def produce_index(cls, path, filename):
-    #     index = []
-    #     mbe_filepath = os.path.join(path, filename)
-    #     with open(mbe_filepath, 'r', encoding='utf8') as F:
-    #         F.readline()
-    #         id_len = id_lengths.get('/'.join(splitpath(mbe_filepath)[-3:]), 1)
-    #         csvreader = csv.reader(F, delimiter=',', quotechar='"')
-    #         for line in csvreader:
-    #             record_id = [item.strip() for item in line[:id_len]]
-    #             index.append(record_id)
-    #     return cls.group, os.path.join(path, filename), [tuple(idx) for idx in index]
 
     @staticmethod
     def get_pack_name(filepath):
         return os.path.split(filepath)[0]
+    
+    @classmethod
+    def get_filetype_cls(cls):
+        return MBE_table
+    
+    
+class MBE_table(BaseFiletype):
+    __slots__ = tuple()
+    
+    build_elements = [MBETableBuildElement]
+    filetype_id = "mbetable"
+    filepack = "MBE"
+    
+    @staticmethod
+    def checkIfMatch(path, filename):
+        if os.path.split(path)[-1].split('.')[-1] == 'mbe' and os.path.splitext(filename)[-1] == '.csv':
+            return True
+        else:
+            return False
