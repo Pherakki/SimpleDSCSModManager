43258969ae99f44e486772199b5eb43db2753c4c
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-03-25 17:31:52 +0000

    Update the sqmod filetype to use the new base class types

--- plugins/filetypes/SQMOD.py
+++ plugins/filetypes/SQMOD.py
@@ -1,16 +1,12 @@
 import os
+from CoreOperations.PluginLoaders.FiletypesPluginLoader import BaseBuildElement, BaseFiletype
 
-class SqMod:
-    group = 'sqmod'
-    default_rule = 'squirrel_modify'
-    enable_softcodes = False
+class SqModBuildElement(BaseBuildElement):
+    __slots__ = tuple()
     
-    @staticmethod
-    def checkIfMatch(path, filename):
-        if os.path.split(path)[-1] == 'script64' and os.path.splitext(filename)[-1] == '.sqmod':
-            return True
-        else:
-            return False
+    build_element_id = 'sqmod'
+    default_rule = 'squirrel_modify'
+    enable_softcodes = True
     
     @staticmethod
     def get_target(filepath):
@@ -19,11 +15,26 @@ class SqMod:
     @classmethod
     def get_rule(cls, filepath):
         return cls.default_rule
-    
-    @classmethod
-    def produce_index(cls, path, filename):
-        return cls.group, os.path.join(path, filename), [filename]
 
     @staticmethod
     def get_pack_name(filepath):
         return filepath
+
+    @classmethod
+    def get_filetype_cls(cls):
+        return SqMod
+    
+    
+class SqMod(BaseFiletype):
+    __slots__ = tuple()
+    
+    build_elements = [SqModBuildElement]
+    filetype_id = "sqmod"
+    
+    @staticmethod
+    def checkIfMatch(path, filename):
+        if os.path.split(path)[-1] == 'script64' and os.path.splitext(filename)[-1] == '.sqmod':
+            return True
+        else:
+            return False
+    
