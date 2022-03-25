317e9d33af94ef1fe3486167e6d7ed92889b5734
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-03-25 17:13:44 +0000

    Fix the softcode regex patterns to include additional characters

--- CoreOperations/ModRegistry/Softcoding.py
+++ CoreOperations/ModRegistry/Softcoding.py
@@ -7,15 +7,17 @@ import re
 # [A::B|C::D|E::F]
 # [A::B|C::D|E::F::G()]
 # etc.
-expr_text = b""
-expr_text += rb"(?<=\[)"                              # Match [, do not include it in result
-expr_text += rb"([a-zA-Z0-9\s]+::[a-zA-Z0-9\s]+\|)*"  # Repeatedly match pairs of alphanumeric 'words' separated by :: and ending with |
-expr_text += rb"([a-zA-Z0-9\s]+::[a-zA-Z0-9\s]+)"     # Match a pair of alphanumeric 'words' separated by :: once
-expr_text += rb"(::[a-zA-Z0-9\s]+\(\))?"              # Optionally match an alphanumeric word beginning with :: and ending with ()
-expr_text += rb"(?=\])"                               # Match ], do not include it in result
+capture_block = r"[a-zA-Z0-9\s_]"
+expr_text = ""
+expr_text += r"(?<=\[)"                                             # Match [, do not include it in result
+expr_text += r"({}+::{}+\|)*".format(capture_block, capture_block)  # Repeatedly match pairs of alphanumeric 'words' separated by :: and ending with |
+expr_text += r"({}+::{}+)".format(capture_block, capture_block)     # Match a pair of alphanumeric 'words' separated by :: once
+expr_text += r"(::{}+\(\))?".format(capture_block)                  # Optionally match an alphanumeric word beginning with :: and ending with ()
+expr_text += r"(?=\])"                                              # Match ], do not include it in result
 
-expr =   re.compile(expr_text.decode('ascii'))
-b_expr = re.compile(expr_text)
+expr =   re.compile(expr_text)
+b_expr = re.compile(expr_text.encode('ascii'))
+
 def search_string_for_softcodes(input_string):
     return expr.finditer(input_string)
 
