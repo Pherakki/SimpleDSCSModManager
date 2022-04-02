16738da4cbe4c68501ad6ff24dcad1cb0b55bbe0
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-04-02 21:11:41 +0100

    Fix translationStringsUpdate to work with the number of files we now have

--- translationStringsUpdate.bat
+++ translationStringsUpdate.bat
@@ -1,12 +1,16 @@
 @ECHO off
 SETLOCAL enabledelayedexpansion
 
-SET py_files = ""
+TYPE NUL >pyfiles.txt
+
 FOR /r %%f IN (*.py) DO (
-    SET py_files=!py_files! "!%%f!"
+    ECHO | SET /p=%%f>>pyfiles.txt
+    ECHO | SET /p= >>pyfiles.txt
 )
 
 FOR %%f IN (./languages/src/*.ts) DO ( 
     ECHO Updating %%f...
-    pylupdate5 %py_files% -ts ./languages/src/%%f
+    pylupdate5 <pyfiles.txt -ts ./languages/src/%%f
 )
+
+DEL pyfiles.txt
