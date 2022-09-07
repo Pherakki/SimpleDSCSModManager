818e0c79b9f8bb5192a19ddccf7727df53a67fbd
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-09-07 20:42:36 +0100

    Fix name detection not taking builtin styles into account

--- UI/StyleEngine.py
+++ UI/StyleEngine.py
@@ -100,7 +100,7 @@ class StyleEngine:
             return name + " 2"
         
     def generate_new_style_name(self, name):
-        if name in self.styles:
+        if name in self.styles or name in self.builtin_styles:
             name = self.generate_style_name(name)
         return name
 
