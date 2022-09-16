36f4a640ad450ef7a6edb518551b88b0caacd1b2
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-09-16 18:51:11 +0100

    Add setup.py for SDMM

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+
+# Build DSCSTools
+os.chdir("sdmmlib")
+os.chdir("dscstools")
+os.system("python setup.py build_ext --inplace")
+os.chdir(os.path.pardir)
+os.chdir(os.path.pardir)
+
+# Build Squirrel
+os.chdir("sdmmlib")
+os.chdir("squirrel")
+os.system("python setup.py build_ext --inplace")
+os.chdir(os.path.pardir)
+os.chdir(os.path.pardir)
+
+# Build Squirrel
+os.chdir("sdmmlib")
+os.chdir("nutcracker")
+os.system("python setup.py build_ext --inplace")
+os.chdir(os.path.pardir)
+os.chdir(os.path.pardir)
+
+print("Copying structures...")
+shutil.copytree(os.path.join("sdmmlib", "dscstools", "DSCSTools", "DSCSTools", "structures"), 
+                os.path.join(os.path.curdir, "structures"))
+print("Setup complete.")
