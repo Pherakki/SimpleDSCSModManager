24165edea87a5bdf60069404d8babcc136c9b9b3
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-09-17 16:01:51 +0100

    Handle case where structure files already exist in root

--- setup.py
+++ setup.py
@@ -23,6 +23,11 @@ os.chdir(os.path.pardir)
 os.chdir(os.path.pardir)
 
 print("Copying structures...")
-shutil.copytree(os.path.join("sdmmlib", "dscstools", "DSCSTools", "DSCSTools", "structures"), 
-                os.path.join(os.path.curdir, "structures"))
+
+structure_src = os.path.join("sdmmlib", "dscstools", "DSCSTools", "DSCSTools", "structures")
+structure_dst = os.path.join(os.path.curdir, "structures")
+if os.path.isdir(structure_dst):
+    shutil.rmtree(structure_dst)
+shutil.copytree(structure_src, structure_dst)
 print("Setup complete.")
+
